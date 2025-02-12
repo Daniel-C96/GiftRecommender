1123cbb77ff17c5b6a6f16e0ce589f59a7346266
Daniel C <Danielcontaf1996@gmail.com>
2025-02-12 22:58:52 +0100

    added restrictions in the geminiDTO

--- src/main/java/com/example/GiftRecommender/dto/GeminiRequest.java
+++ src/main/java/com/example/GiftRecommender/dto/GeminiRequest.java
@@ -1,18 +1,18 @@
 package com.example.GiftRecommender.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class GeminiRequest {
 
-    //@NotNull(message = "Age is null.")
+    @Min(value = 0, message = "Age cannot be less than 0.")
+    @Max(value = 150, message = "Age cannot be greater than 150.")
     private int age;
 
-    //@NotNull(message = "Gender is null.")
-    //@NotEmpty(message = "Gender is empty.")
     @Size(max = 15)
     private String gender;
 
@@ -22,17 +22,27 @@ public class GeminiRequest {
     @Size(max = 20)
     private String who;
 
-    //@NotNull(message = "Hobbies is null.")
-    //@NotEmpty(message = "Hobbies is empty.")
     private String[] hobbies;
 
     @Size(max = 100)
     private String description;
 
+    @Min(value = 0, message = "Minimum budget cannot be less than 0.")
+    @Max(value = 100_000_000, message = "Minimum budget cannot be greater than 100,000,000.")
     private Integer minBudget;
 
+    @Min(value = 0, message = "Maximum budget cannot be less than 0.")
+    @Max(value = 100_000_000, message = "Maximum budget cannot be greater than 100,000,000.")
     private Integer maxBudget;
 
     private String language;
 
+    // Ensures that maxBudget is not smaller than minBudget.
+    @AssertTrue(message = "Maximum budget cannot be smaller than the minimum budget.")
+    public boolean isValidBudget() {
+        if (minBudget != null && maxBudget != null) {
+            return maxBudget >= minBudget;
+        }
+        return true;
+    }
 }
